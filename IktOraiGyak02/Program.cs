ba8e1bf62c55ab036967f63de64e1326a42ceb6e
= <=>
2024-09-11 08:20:12 +0200

    Add project files.

---
+++ IktOraiGyak02/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+if (args.Length > 1 && args.Length % 2 == 1)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine(arg);
+    }
+}
+else
+{
+    Console.WriteLine("Nem megfelelő argumentumszám!");
+}
